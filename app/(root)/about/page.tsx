40930501850c2b4ac62edd5621d13445edae36d2
kerryl02 <b.soda973@gmail.com>
2025-09-10 05:36:40 +0200

    pages root & page error :egg:

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  throw new Error("NOT IMPLEMENTED")
+  return (
+    <div>about</div>
+  )
+}
+
+export default page
---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  console.log('What am i ')
+  return (
+    <h1 className="text-3xl"> welcome to nexjs</h1>
+  );
+}
